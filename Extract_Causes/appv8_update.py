f1b06990a5a0d6a94704444769011b77df55bdc8
Shivakumar <35722090+ShivakumarKoti@users.noreply.github.com>
2024-12-26 19:30:25 +0530

    Update appv8_update.py

--- Extract_Causes/appv8_update.py
+++ Extract_Causes/appv8_update.py
@@ -13,7 +13,7 @@ symptom_list = [
 'low blood pressure', 'excessive thirst', 'dehydration', 'skin burning', 'sweat', 'eye pain', 'red eyes', 'eye discharge', 'ear discharge', 'jaundice',
 'hearing loss', 'balance problem', 'irregular heartbeat', 'fainting', 'tremor', 'nervousness', 'panic attack', 'mood swing', 'difficulty concentrating',
 'hallucination', 'lack of motivation', 'exhaustion', 'bone pain', 'wrist pain', 'sprain', 'strain', 'arthritis', 'gout', 'headache', 'injury', 'chills',
-'leg pain', 'hand pain', 'arm pain', 'foot pain', 'knee pain', 'shoulder pain', 'hip pain', 'jaw pain', 'tooth pain','sleepy'
+'leg pain', 'hand pain', 'arm pain', 'foot pain', 'knee pain', 'shoulder pain', 'hip pain', 'jaw pain', 'tooth pain','sleepy', 'bone fracture'
 ]
 
 # Remove duplicates by converting the list to a set and back to a list
